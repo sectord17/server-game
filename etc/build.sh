ab4921b55cac5c0dee0abecd4bd75a24dd9c9017
Michał Budziak <michal.mariusz.b@gmail.com>
2017-11-01 16:51:14 +0100

    Change decide to decision

--- etc/build.sh
+++ etc/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-cp .env.example .env
+#cp .env.example .env
 npm install
-MOCHA_FILE=./jenkins-test-results.xml ./node_modules/.bin/mocha test/** --reporter mocha-junit-reporter
+MOCHA_FILE=./jenkins-test-results.xml IP=127.0.0.1 GAME_PORT=0 HTTP_PORT=0 TOKEN=secret \
+./node_modules/.bin/mocha test/** --reporter mocha-junit-reporter
