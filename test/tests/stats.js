d9b8c39d716b3225e8871276a445bd0775c8e061
Michał Budziak <michal.mariusz.b@gmail.com>
2017-11-24 18:26:04 +0100

    Add test for checking for game finish

--- test/tests/stats.js
+++ test/tests/stats.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const {beforeEach, describe, it} = require('mocha');
 const flatbuffers = require('flatbuffers').flatbuffers;
+const RoomAssets = require('../../src/flatbuffers/RoomSchema_generated').Assets;
 const GameAssets = require('../../src/flatbuffers/GameSchema_generated').Assets;
 const {createPlayer, startGame, beforeEach: setupBeforeEach} = require('../utils/helpers');
 const {statsManager} = require('../../src');
@@ -31,6 +32,33 @@ describe('Statistics on server', function () {
                             assert.equal(reason.targetId(), connections2.player.id);
 
                             done();
+                        }
+                    }
+                }));
+            })
+            .catch(error => done(error));
+    });
+
+    it('game ends when player gains 30 points', function (done) {
+        Promise.all([createPlayer(), createPlayer()])
+            .then(startGame)
+            .then(([connections1, connections2]) => {
+                statsManager.onPlayerDeath(connections1.player, connections2.player);
+                statsManager.onPlayerDeath(connections1.player, connections2.player);
+                statsManager.onPlayerDeath(connections2.player, connections1.player);
+                statsManager.onPlayerDeath(connections1.player, connections2.player);
+
+                connections1.clientTcp.on('data', data => splitData(data, data => {
+                    const message = new Uint8Array(data);
+                    const buf = new flatbuffers.ByteBuffer(message);
+
+                    if (RoomAssets.Code.Remote.Flat.RoomMsg.bufferHasIdentifier(buf)) {
+                        const roomMsg = RoomAssets.Code.Remote.Flat.RoomMsg.getRootAsRoomMsg(buf);
+                        if (roomMsg.dataType() === RoomAssets.Code.Remote.Flat.RoomData.GameStatusChanged) {
+                            const gameStatusChanged = roomMsg.data(new RoomAssets.Code.Remote.Flat.GameStatusChanged());
+                            if (gameStatusChanged.status() === RoomAssets.Code.Remote.Flat.GameStatus.Finish) {
+                                done();
+                            }
                         }
                     }
                 }));
